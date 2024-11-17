7a5a956bc77fd7608e7ba6587b6a056162a2e4ed
WangShaoze <2847832624@qq.com>
2024-11-17 15:45:48 +0800

    桥模式

--- bridge_pattern/bridge_pattern.py
+++ bridge_pattern/bridge_pattern.py
@@ -6,18 +6,22 @@
 第三个维度: 粗细
 """
 from abc import ABCMeta, abstractmethod
+
+
 class Color(metaclass=ABCMeta):
     @abstractmethod
     def paint(self, shape):
         pass
+
 
 class Thickness(metaclass=ABCMeta):
     @abstractmethod
     def discript(self, shape):
         pass
 
+
 class Shape(metaclass=ABCMeta):
-    def __init__(self, color:Color, thickness:Thickness):
+    def __init__(self, color: Color, thickness: Thickness):
         self.color = color
         self.thickness = thickness
 
@@ -25,37 +29,45 @@ class Shape(metaclass=ABCMeta):
     def draw(self):
         pass
 
+
 class Green(Color):
     def paint(self, shape):
         return "绿"
+
 
 class Red(Color):
     def paint(self, shape):
         return "红"
 
+
 class Black(Color):
     def paint(self, shape):
         return "黑"
+
 
 class ThinLine(Thickness):
     def discript(self, shape):
         return "细"
 
+
 class ThickLine(Thickness):
     def discript(self, shape):
         return "粗"
 
+
 class Circle(Shape):
     def draw(self):
-        print("用{}线画{}色圆".format(self.thickness.discript(self),self.color.paint(self)))
+        print("用{}线画{}色圆".format(self.thickness.discript(self), self.color.paint(self)))
+
 
 class Rectangle(Shape):
     def draw(self):
-        print("用{}线画{}色正方形".format(self.thickness.discript(self),self.color.paint(self)))
+        print("用{}线画{}色正方形".format(self.thickness.discript(self), self.color.paint(self)))
+
 
 class Heart(Shape):
     def draw(self):
-        print("用{}线画{}色心形".format(self.thickness.discript(self),self.color.paint(self)))
+        print("用{}线画{}色心形".format(self.thickness.discript(self), self.color.paint(self)))
 
 
 if __name__ == "__main__":
