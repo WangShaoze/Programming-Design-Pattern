9109afb3d105a3ff822e3ff9614cbd432bb13d88
WangShaoze <2847832624@qq.com>
2024-11-17 15:52:12 +0800

    策略模式

--- strategy_pattern/strategy_pattern_after.py
+++ strategy_pattern/strategy_pattern_after.py
@@ -1,40 +1,53 @@
 from abc import ABC, abstractmethod
 
+
 class StrategyProcesser(ABC):
     """ 文件处理策略接口 """
+
     @abstractmethod
-    def read_data(self, file_path:str):
+    def read_data(self, file_path: str):
         pass
+
 
 class XLSXExcelProcesser(StrategyProcesser):
     """ 处理 专门的XLSX 文件的类 """
-    def read_data(self, file_path:str):
-            print("正在解析:xlsx 的Excel文件")
+
+    def read_data(self, file_path: str):
+        print("正在解析:xlsx 的Excel文件")
 
 
 class XLSExcelProcesser(StrategyProcesser):
     """ 处理 专门的XLS 文件的类 """
-    def read_data(self, file_path:str):
-            print("正在解析:xls 的Excel文件")
+
+    def read_data(self, file_path: str):
+        print("正在解析:xls 的Excel文件")
+
 
 class TXTProcesser(StrategyProcesser):
     """ 处理 专门的TXT 文件的类 """
-    def read_data(self, file_path:str):
-            print("正在解析: TXT 文件")
+
+    def read_data(self, file_path: str):
+        print("正在解析: TXT 文件")
+
 
 class CSVProcesser(StrategyProcesser):
     """ 处理 专门的CSV 文件的类 """
-    def read_data(self, file_path:str):
-            print("正在解析: CSV 文件")
+
+    def read_data(self, file_path: str):
+        print("正在解析: CSV 文件")
+
 
 class JSONProcesser(StrategyProcesser):
     """ 处理 专门的 JSON 文件的类 """
-    def read_data(self, file_path:str):
-            print("正在解析: JSON 文件")
+
+    def read_data(self, file_path: str):
+        print("正在解析: JSON 文件")
+
 
 class FileProcesser:
     """  文件处理类 """
-    def __init__(self, file_path:str):
+
+    def __init__(self, file_path: str):
         self.file_path = file_path
         self.file_name = None
         self.file_type = None
@@ -45,8 +58,7 @@ class FileProcesser:
         if "\\" in self.file_path:
             tmp = self.file_path.split("\\")[-1]
         self.file_type = tmp.split(".")[1]
-        self.file_name  = tmp.split(".")[0]
-
+        self.file_name = tmp.split(".")[0]
 
     def read_data(self):
         if self.file_type == "xlsx":
